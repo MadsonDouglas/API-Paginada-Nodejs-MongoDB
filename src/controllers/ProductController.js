74d74129cf27e97b0b12dfe0db9afb9bb23d99bb
MadsonDouglas <santos.madson94@gmail.com>
2019-06-09 16:01:33 -0300

    adicionando o gitignore

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,7 +5,7 @@ const Product = mongoose.model('Product')
 module.exports = {
     async findALL(req, res) {
         const { page = 1} = req.query    
-        const products = await Product.paginate({}, { page, limit: 5 })
+        const products = await Product.paginate({},{ page, limit: 5 })
         return res.json(products)
     },
     async show(req, res) {
@@ -17,7 +17,11 @@ module.exports = {
         return res.json(product)
     },
     async update(req, res) {
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        const product = await Product.findByIdAndUpdate(
+            req.params.id, 
+            req.body,
+            { new: true })
+
         return res.json(product)
     },
     async destroy(req, res) {
